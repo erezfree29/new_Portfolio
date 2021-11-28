a53630559d48cf227d9bed15fdb9f749d3b8c741
Erez Friemagor <erezfree29@gmail.com>
2021-11-28 19:08:47 +0000

    nearly completed

---
+++ script.js
@@ -0,0 +1,29 @@
+setInterval(function(){ 
+
+    let x = document.querySelector('.web_developer');
+    let y = document.querySelector('.ruby_developer');
+    let  z = document.querySelector('.java_script');
+    let  r = document.querySelector('.react');
+    let  red = document.querySelector('.redux');
+    if (x.style.display === "block") {
+      x.style.display = "none";
+      y.style.display = "block";
+
+    } else if (y.style.display === "block"){
+      y.style.display = "none";
+      z.style.display = "block";
+    } else if (z.style.display === "block"){
+      r.style.display = "block";
+      z.style.display = "none";
+    }
+    else if (r.style.display === "block") {
+      r.style.display = "none";
+      red.style.display = "block";
+    }
+
+    else {
+      red.style.display = "none";
+      x.style.display = "block";
+    }
+
+}, 3000);
