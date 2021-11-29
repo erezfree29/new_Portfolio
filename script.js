03a7d945bef10e5c6651b4e9e6d44ea1c6fd486b
Erez Friemagor <erezfree29@gmail.com>
2021-11-29 09:27:49 +0000

    nearly completed

--- script.js
+++ script.js
@@ -42,5 +42,101 @@ function hideShow() {
     }
   }
 
+  function showSkills() {
+    document.getElementById("skills").style.display = 'block';
+    document.getElementById("projects_content").style.display = 'none';
+  }
 
-setInterval(function(){ hideShow(); }, 50000);
+  function showSkillsMobile() {
+    document.querySelector(".cards_mobile").style.display = 'flex';
+    document.getElementById("projects_content").style.display = 'none';
+    document.querySelector('.side_bar').style.display = 'none';
+  }
+
+  function showProjectsMobile() {
+    document.querySelector(".cards_mobile").style.display = 'none';
+    document.getElementById("projects_content").style.display = 'flex';
+    document.querySelector('.side_bar').style.display = 'none';
+  }
+
+
+
+  function showProjects() {
+    document.getElementById("skills").style.display = 'none';
+    document.getElementById("projects_content").style.display = 'block';
+    document.querySelector('.side_bar').style.display('none');
+  }
+
+
+  function showSideBar() {
+    document.getElementById("skills").style.display = 'none';
+    document.getElementById("projects_content").style.display = 'none';
+    document.querySelector('.side_bar').style.display = 'block';
+  }
+
+
+
+  setInterval(function(){ hideShow(); }, 50000);
+
+var x = window.matchMedia("(min-width: 768px)")
+var y = window.matchMedia("(max-width: 768px)")
+
+if (x.matches) { // If media query matches
+    document.getElementById('show_skills').addEventListener("click", showSkills);
+    document.getElementById('show_projects').addEventListener("click", showProjects);
+}
+
+
+
+
+function changeDivsMobile() {
+   x =  document.querySelector('.side_bar');
+   y =  document.querySelector('.cards_mobile');
+   z =  document.getElementById('projects_content');
+   if (x.style.display === "block") {
+    x.style.display = "none";
+    y.style.display = "flex";
+
+  } else if (y.style.display === "flex"){
+    y.style.display = "none";
+    z.style.display = "block";
+  } else {
+    z.style.display = "none";
+    x.style.display = "block";
+  }
+
+  }
+
+
+
+if (y.matches) { // If media query matches
+    document.getElementById('show_skills').addEventListener("click", showSkillsMobile);
+    document.getElementById('show_projects').addEventListener("click", showProjectsMobile);
+    document.querySelector('.show_side').addEventListener("click", showSideBar);
+    setInterval(function(){ changeDivsMobile(); }, 10000);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
